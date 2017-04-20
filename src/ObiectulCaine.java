b02bd2d26e1707a3b5e03012a677e616927136fd
olimpialarionesi <olimpialarionesi@gmail.com>
2017-04-20 15:28:09 +0300

    Chick

--- src/ObiectulCaine.java
+++ src/ObiectulCaine.java
@@ -31,3 +31,17 @@ public class ObiectulCaine {
 
     }
 }
+/*pubilc class ObiectulCaine{
+private String nume;
+private String varsta;
+public String getNume(){ return nume;}
+public void setNume(String nume){ this.nume = nume;}
+pubic string getVarsta() {return varsta;}
+
+public void setVarsta(String varsta) { this.varsta = varsta;}
+
+ObiectulCaine ( String nume, String varsta){
+this.nume = nume;
+this.varsta = varsta;
+
+ */
